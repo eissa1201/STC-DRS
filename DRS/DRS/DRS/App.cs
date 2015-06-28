613115e498ac1cdbea920d981bec17f711490c7e
Ahmed Al-Eissa <Eissa1201@hotmail.com>
2015-06-28 18:04:41 +0300

    DRS version 0.0
    First edition of the DRS project

---
+++ DRS/DRS/DRS/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DRS
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = GetMainPage();
+        }
+
+
+        public Page GetMainPage()
+        {
+
+
+            return new NavigationPage(new WelcomePage());
+
+
+        }
+
+    }
+}
